8bcb4a45d55ed1c4af2a53d4d2b37337e576c2a1
arif <hossaininfo@ymail.com>
2022-04-25 14:56:58 +0200

    Initial commit

---
+++ src/main/java/com/foodbear/foodbear/FoodBearApplication.java
@@ -0,0 +1,13 @@
+package com.foodbear.foodbear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBearApplication.class, args);
+	}
+
+}
