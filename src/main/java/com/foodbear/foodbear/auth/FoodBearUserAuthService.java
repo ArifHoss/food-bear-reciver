5f1cbad7493bb20b1196bed744cf1afa0f6ad856
karinismael <karin.ismael@hotmail.com>
2022-05-05 20:35:59 +0200

    user log-in, passwordencoder

---
+++ src/main/java/com/foodbear/foodbear/auth/FoodBearUserAuthService.java
@@ -0,0 +1,27 @@
+package com.foodbear.foodbear.auth;
+
+import com.foodbear.foodbear.entities.FoodBearUser;
+import com.foodbear.foodbear.repo.FoodBearUserDaoJpa;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FoodBearUserAuthService implements UserDetailsService {
+
+    private final FoodBearUserDaoJpa foodBearUserDaoJpa;
+
+    public FoodBearUserAuthService(FoodBearUserDaoJpa foodBearUserDaoJpa) {
+        this.foodBearUserDaoJpa = foodBearUserDaoJpa;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        FoodBearUser foodBearUser = foodBearUserDaoJpa.findByEmail(email);
+        if (foodBearUser == null) {
+            throw new UsernameNotFoundException("Can't find user with email: " + email);
+        }
+        return new FoodBearPrincipal(foodBearUser);
+    }
+}
---
+++ src/main/java/com/foodbear/foodbear/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.foodbear.foodbear.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final FoodBearUserAuthService foodBearUserAuthService;
+
+    public SecurityConfig(FoodBearUserAuthService foodBearUserAuthService) {
+        this.foodBearUserAuthService = foodBearUserAuthService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(foodBearUserAuthService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","/user").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
