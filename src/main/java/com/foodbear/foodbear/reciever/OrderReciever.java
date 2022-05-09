0e3868b160e9e8ff1140b992cbfcda992fab208f
arif <hossaininfo@ymail.com>
2022-05-09 15:30:09 +0200

    applied Reciever

--- src/main/java/com/foodbear/foodbear/reciever/OrderReciever.java
+++ src/main/java/com/foodbear/foodbear/reciever/OrderReciever.java
@@ -1,21 +1,18 @@
-/*
 package com.foodbear.foodbear.reciever;
 
-import com.foodbear.foodbear.sender.Sender;
+import com.foodbear.foodbear.entities.FoodOrder;
+import com.foodbear.foodbear.jmsconfig.JmsConfig;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class OrderReciever {
 
-    private static JmsTemplate jmsTemplate;
-    public static final String FOODBEAR_QUEUE = "foodbear-queue";
-
-    @JmsListener(destination = Sender.FOODBEAR_QUEUE)
-    public static String orderListener(){
-         return (String) jmsTemplate.receiveAndConvert(FOODBEAR_QUEUE);
+    @JmsListener(destination = JmsConfig.FOODBEAR_QUEUE)
+    public void  orderListener(@Payload FoodOrder foodOrder){
+        System.out.println("I got a order");
+        System.out.println(foodOrder);
     }
 }
-*/
